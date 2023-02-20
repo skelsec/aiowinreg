81123d908e6dc01ea3e69b710173040b69e723b0
SkelSec <skelsec@skelsecprojects.com>
2023-02-20 14:31:39 -0800

    minor cosmetics for build script

--- builder/pyinstaller/build.bat
+++ builder/pyinstaller/build.bat
@@ -1,13 +1,19 @@
-@echo off
+
 set root=%~dp0
 set projectname=aiowinreg
-set pyenv=%root%\env
 set repo=%root%..\..\%projectname%
-python -m venv %pyenv%
-%pyenv%\Scripts\activate.bat &^
-pip install pyinstaller &^
+IF NOT DEFINED __BUILDALL_VENV__ (
+    set pyenv=%root%\env
+    python -m venv %pyenv%
+    %pyenv%\Scripts\activate.bat &^
+    pip install pyinstaller
+) &^
 cd %repo%\..\ &^
 pip install . &^
 cd %repo%\ &^
 pyinstaller -F __main__.py %hiddenimports% &^
-cd %repo%\dist & copy __main__.exe %root%\winregconsole.exe
+cd %repo%\dist & copy __main__.exe %root%\winregconsole.exe &^
+IF NOT DEFINED __BUILDALL_VENV__ (
+    deactivate
+) &^
+cd %root%
