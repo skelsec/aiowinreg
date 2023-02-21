285a924854344d4167cce06a6388786473178d23
SkelSec <skelsec@skelsecprojects.com>
2023-02-21 03:07:50 -0800

    builder again...

--- builder/pyinstaller/build.bat
+++ builder/pyinstaller/build.bat
@@ -3,11 +3,9 @@ set root=%~dp0
 set projectname=aiowinreg
 set repo=%root%..\..\%projectname%
 IF NOT DEFINED __BUILDALL_VENV__ (
-set pyenv=%root%\env
-python -m venv %pyenv%
-%pyenv%\Scripts\activate.bat &^
-pip install pyinstaller
-) &^
+python -m venv %root%\env
+%root%\env\Scripts\activate.bat &^
+pip install pyinstaller ) &^
 cd %repo%\..\ &^
 pip install . &^
 cd %repo%\ &^
