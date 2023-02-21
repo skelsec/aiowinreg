2b613540b7982136bfacbdcefdc4c46e55d0d0e9
SkelSec <skelsec@skelsecprojects.com>
2023-02-21 07:42:05 -0800

    final build

--- builder/pyinstaller/build.bat
+++ builder/pyinstaller/build.bat
@@ -1,17 +1,26 @@
-
-set root=%~dp0
+@echo off
 set projectname=aiowinreg
+set root=%~dp0
 set repo=%root%..\..\%projectname%
-IF NOT DEFINED __BUILDALL_VENV__ (
+
+IF NOT DEFINED __BUILDALL_VENV__ (GOTO :CREATEVENV)
+GOTO :BUILD
+
+:CREATEVENV
 python -m venv %root%\env
-%root%\env\Scripts\activate.bat &^
-pip install pyinstaller ) &^
-cd %repo%\..\ &^
-pip install . &^
-cd %repo%\ &^
-pyinstaller -F __main__.py %hiddenimports% &^
-cd %repo%\dist & copy __main__.exe %root%\winregconsole.exe &^
-IF NOT DEFINED __BUILDALL_VENV__ (
-deactivate
-) &^
+CALL %root%\env\Scripts\activate.bat
+pip install pyinstaller
+GOTO :BUILD
+
+:BUILD
+cd %repo%\..\
+pip install .
+cd %repo%\
+pyinstaller -F __main__.py %hiddenimports%
+cd %repo%\dist & copy __main__.exe %root%\winregconsole.exe
+GOTO :CLEANUP
+
+:CLEANUP
+IF NOT DEFINED __BUILDALL_VENV__ (deactivate)
 cd %root%
+EXIT /B
